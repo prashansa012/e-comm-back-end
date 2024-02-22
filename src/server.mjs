84ca1ae56293d9727c64f0ec1529e8a38151e5e0
prashansa012 <156915156+prashansa012@users.noreply.github.com>
2024-02-22 11:41:15 +0530

    set up an express server , test server with postman , route perameter in express, get data from a server

--- src/server.mjs
+++ src/server.mjs
@@ -1,10 +1,16 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/hello',(req,res) => {
     res.send('Hello!');
 });
-app.listen(8000,() => {
-    console.log('server is listening on port 8000');
+
+app.post('/hello',(req, res)=>{
+  res.send(`HELLO ${req.body.name}`)
+});
+app.listen(8001,() => {
+    console.log('server is listening on port 8001');
 })
